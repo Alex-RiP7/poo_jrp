f123bcc686a89a70f82bb9cd708b6d347583186e
Alex-RiP7 <1725110975@utectulancingo.edu.mx>
2026-05-17 22:37:12 -0600

    Update silla.py

--- ejercicios/semana_02/silla.py
+++ ejercicios/semana_02/silla.py
@@ -1,1 +1,13 @@
-
+clas Silla:
+   def __init__(self, material, color, altura, peso, tipo, respaldo, ruedas,
+               capacidad_carga, precio, fabricante):
+       self.material = material
+       self.color = color
+       self.altura = altura
+       self.peso = peso
+       self.tipo = tipo
+       self.respaldo = respaldo
+       self.ruedas = ruedas
+       self.capacidad_carga = capacidad_carga
+       self.precio = precio
+       self.fabricante = fabricante
